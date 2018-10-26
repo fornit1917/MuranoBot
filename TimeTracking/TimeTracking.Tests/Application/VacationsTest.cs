d06c4076381327e853fa0ca6f3c02645019c2629
Maxim Rozhkov <mrozhkov@appulate.com>
2018-10-26 21:40:47 +0300

    renaming

--- TimeTracking/TimeTracking.Tests/Application/VacationsTest.cs
+++ TimeTracking/TimeTracking.Tests/Application/VacationsTest.cs
@@ -15,9 +15,9 @@ using MuranoBot.TimeTracking.App.Infrastructure.Repositories;
 
 namespace MuranoBot.TimeTracking.Tests {
 	[TestClass]
-	public class TimeTrackingAppTest {
+	public class VacationsAppTest {
 
-		public TimeTrackingAppTest() {
+		public VacationsAppTest() {
 			var appConfig = AppConfig.Instance;
 			appConfig.TimeTrackerConnectionString = @"Server=localhost;Database=TimeTrackerNew;Trusted_Connection=True;";
 		}
@@ -33,7 +33,7 @@ namespace MuranoBot.TimeTracking.Tests {
 				DateTime to = new DateTime(2018, 09, 12);
 				dbContext.Vacations.Add(new Vacation(userId, from, to));
 				dbContext.SaveChanges();
-				var app = new TimeTrackingApp(new VacationsRepository(dbContext));
+				var app = new VacationsApp(new VacationsRepository(dbContext));
 
 				// Act
 				var rc = app.GetVacationInfo(userId, new DateTime(2018, 08, 27));
@@ -52,7 +52,7 @@ namespace MuranoBot.TimeTracking.Tests {
 				int userId = 1091;
 				DateTime from = new DateTime(2018, 08, 24);
 				DateTime to = new DateTime(2018, 09, 12);
-				var app = new TimeTrackingApp(new VacationsRepository(dbContext));
+				var app = new VacationsApp(new VacationsRepository(dbContext));
 
 				// Act
 				app.SetVacation(new VacationInfo {
