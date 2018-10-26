f2217bd5e628dc98e83ae6dd6b5b8a32edd7b375
Vit S <fornit1917@gmail.com>
2018-10-26 22:06:31 +0300

    check if chat accound bound

--- Messengers/Models/BotRequest.cs
+++ Messengers/Models/BotRequest.cs
@@ -9,6 +9,7 @@ namespace Messengers.Models
         public Messenger Messenger { get; set; }
         public string UserId { get; set; }
         public string ChannelId { get; set; }
+        public bool IsDirectMessage { get; set; }
         public string Text { get; set; }
     }
 }
