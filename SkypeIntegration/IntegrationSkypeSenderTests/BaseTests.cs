aa5b3d0f8d4d93b3309003038ca6ce2bcdaa596b
Eugene <etikhonov@appulate.com>
2018-10-27 05:41:34 +0300

    add priority queue for sending to skype

--- SkypeIntegration/IntegrationSkypeSenderTests/BaseTests.cs
+++ SkypeIntegration/IntegrationSkypeSenderTests/BaseTests.cs
@@ -1,18 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SkypeIntegration.Skype;
 
 namespace IntegrationSkypeSenderTests {
 	[TestClass]
 	public class BaseTests {
-		private SkypeSender _skype;
+		private static SkypeSender _skype;
 
-		[TestInitialize]
-		public void StartService() {
+		[ClassInitialize]
+		public static void StartService(TestContext context) {
 			_skype = new SkypeSender("appulateautomaionshared@appulatemail.com", "pwdSkyMurBot");
 		}
 
-		[TestCleanup]
-		public void ShutDownService() {
+		[ClassCleanup]
+		public static void ShutDownService() {
 			_skype.Dispose();
 		}
 
@@ -22,6 +25,28 @@ namespace IntegrationSkypeSenderTests {
 			Assert.IsTrue(_skype.Write("Public chat", "Hello 2"));
 			Assert.IsTrue(_skype.Write("Public chat", "Hello 3"));
 			Assert.IsTrue(_skype.Write("Eugene Tikhonov", "Hello 4"));
+		}
+
+		[TestMethod]
+		public void AsyncWriteSomeMessagesToSomeChats() {
+			var to = new [] {
+				"Eugene Tikhonov",
+				"Public chat" 
+			};
+
+			var tasks = new List<Task<bool>>();
+			var funcs = new List<Func<bool>>();
+
+			for (var i = 0; i < 10; i++) {
+				var copyI = i;
+				funcs.Add(() => _skype.Write(to[copyI % to.Length], $"async Hello #{copyI}"));
+			}
+
+			funcs.ForEach(f => tasks.Add(Task.Run(f)));
+
+			foreach (var task in tasks) {
+				Assert.IsTrue(task.Result);
+			}
 		}
 	}
 }
