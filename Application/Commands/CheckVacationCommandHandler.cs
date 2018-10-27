9599f9469fa2105961b8a36a1967fb6c4371d1a3
Maxim Rozhkov <mrozhkov@appulate.com>
2018-10-27 07:32:48 +0300

    Add domain name retrieving

--- Application/Commands/CheckVacationCommandHandler.cs
+++ Application/Commands/CheckVacationCommandHandler.cs
@@ -4,19 +4,24 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using MuranoBot.Common;
 using MuranoBot.Domain;
 using MuranoBot.Infrastructure.MessageSenders;
 using MuranoBot.Infrastructure.MessageSenders.Models;
 using MuranoBot.Infrastructure.TimeTracking.App.Application;
+using SlackAPI;
+using SlackAPI.RPCMessages;
 
 namespace MuranoBot.Application.Commands {
 	public class CheckVacationCommandHandler : IRequestHandler<CheckVacationCommand, bool> {
 		private readonly VacationsApp _vacationsApp;
 		private readonly MessageSender _messageSender;
+		private readonly SlackClient _slackClient;
 
 		public CheckVacationCommandHandler(VacationsApp vacationsApp, MessageSender messageSender) {
 			_vacationsApp = vacationsApp;
 			_messageSender = messageSender;
+			_slackClient = new SlackClient(AppConfig.Instance.SlackToken);
 		}
 
 		public Task<bool> Handle(CheckVacationCommand command, CancellationToken cancellationToken) {
@@ -26,19 +31,38 @@ namespace MuranoBot.Application.Commands {
 				Messenger = Messenger.Slack,
 			};
 
+			var realName = GetRealName(command.UserId);
+			var domainName = ConvertToDomainName(realName.FirstName, realName.LastName);
+
 			var rc = _vacationsApp.GetVacationInfo(1091, new DateTime(2018, 08, 27));
 
 			BotResponse botResponse;
 			if (rc != null) {
-				botResponse = new BotResponse { Text = $"'{command.UserId}' в отпуске с '{rc.Interval.Start}' по '{rc.Interval.End}'" };
-			}
-			else {
-				botResponse = new BotResponse { Text = $"'{command.UserId}' не в отпуске" };
+				botResponse = new BotResponse { Text = $"'{domainName}' в отпуске с '{rc.Interval.Start}' по '{rc.Interval.End}'" };
+			} else {
+				botResponse = new BotResponse { Text = $"'{domainName}' не в отпуске" };
 			}
 
 			_messageSender.SendAsync(destination, botResponse);
 
 			return Task.FromResult(true);
+		}
+
+		private (string FirstName, string LastName) GetRealName(string userId) {
+			SlackAPI.User user = null;
+
+			var mre = new ManualResetEvent(false);
+			_slackClient.GetInfo(response => {
+				user = response.user;
+				mre.Set();
+			}, userId);
+			mre.WaitOne();
+
+			return (user.profile.first_name, user.profile.last_name);
+		}
+
+		private string ConvertToDomainName(string firstName, string lastName) {
+			return @"CORP\"+$"{firstName}.{lastName}";
 		}
 	}
 }
